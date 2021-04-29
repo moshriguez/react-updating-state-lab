52dc952677f364e075f43b3f615d58a4dbcf9289
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-04-28 23:54:14 -0700

    commit tree 50800c0de07512843803e1c447c96a23c0282c19

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,40 @@
-// Code YouTubeDebugger Component Here
+import React from 'react'
+import { ContextExclusionPlugin } from 'webpack'
+
+class YouTubeDebugger extends React.Component {
+    state = {
+        errors: [],
+        user: null,
+        settings: {
+            bitrate: 8,
+            video: {
+                resolution: '1080p'
+            }
+        }
+    }
+
+
+    handleBitrate = () => {
+        this.setState({
+            settings: {...this.state.settings,
+            bitrate: 12}
+        })    
+    }
+
+    handleResolution = () => {
+        this.setState({
+            settings: {...this.state.settings,
+                video: {resolution: '720p'}}
+        })}
+    
+
+    render() {
+        return (
+        <div>
+            <button onClick={this.handleBitrate} className="bitrate">Bitrate</button>
+            <button onClick={this.handleResolution} className="resolution">Resolution</button>
+        </div>)
+    }
+}
+
+export default YouTubeDebugger
